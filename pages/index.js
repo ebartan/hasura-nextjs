0635ac4ef6ebdee3b08f8f0980e0fac90ba39972
Eray Bartan <ebartan@gmail.com>
2022-04-06 02:15:30 +0300

    alla alla

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(works) {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +14,17 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hasura with Nextjs
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          {console.log(works)}
+          {works.works.map((work) => (
+            <div key={work.id}>{work.name}</div>
+          ))}
+      
         </div>
+
+
       </main>
 
       <footer className={styles.footer}>
@@ -66,4 +41,39 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
+      // Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query:`query fetchWorks {
+        works {
+          id
+          name
+          description
+          created_at
+          updated_at
+        }
+      }
+      
+      `,
+      variables: {},
+      operationName: "fetchWorks",
+
+    })
+  }
+  const fetchResponse = await fetch('https://ebartanhasuranextjs.hasura.app/v1/graphql',options)
+  const fetchJson = await fetchResponse.json()
+  console.log(fetchJson.data.works)
+  const works = fetchJson.data.works
+  return {
+  
+  props: {works:works }
+  }
 }
